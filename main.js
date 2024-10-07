a534a77657a99915070000ccf8184efddcf3f958
geeg <136278046+geoscreuza@users.noreply.github.com>
2024-10-07 12:16:37 -0300

    gghjkj

--- main.js
+++ main.js
@@ -1,4 +1,17 @@
 function criaCartao (categoria, pergunta, resposta){
 let container = document.getElementById('container');
 let cartao = document.createElement('article');
+cartao.className = 'cartao';
+cartao.innerHTML = '
+``<div class="conteudocartao">
+    <h3>categoria</h3>
+    <div class="pergunta-cartao">
+<p>perguntas</p>
+    </div> 
+     <div class="resposta-cartao">
+<p>respostas</p>
+     </div>
+    </div>
+ '   
+container.appendChild(cartao);
 }
