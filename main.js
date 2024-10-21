6d1f54cf3720fc5c3dc1dc866d912a090a339fb7
geeg <136278046+geoscreuza@users.noreply.github.com>
2024-10-21 09:22:54 -0300

    kf

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ let container = document.getElementById('container');
 let cartao = document.createElement('article');
 cartao.className = 'cartao';
 cartao.innerHTML = `
-<div class="conteudocartao">
+<div class="conteudo-cartao">
         <h3>${categoria}</h3>
     <div class="pergunta-cartao">
 <p>${pergunta}</p>
