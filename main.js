0d14ed1559d06c411b6fbef7a9f24ff9c5b0c94d
geeg <136278046+geoscreuza@users.noreply.github.com>
2024-10-28 10:29:43 -0300

    y42

--- main.js
+++ main.js
@@ -13,5 +13,14 @@ cartao.innerHTML = `
      </div>
     </div>
     `
+let respostaEstaVisivel = false;
+
+function viraCartao(){
+respostaEstaVisivel = !respostaEstaVisivel;
+cartao.classList.toggle ('active', respostaEstaVisivel);
+}
+
+cartao.addEventListener('click', viraCartao)
+
 container.appendChild(cartao);
 }
